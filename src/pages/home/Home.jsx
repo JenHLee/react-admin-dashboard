b57313411d9e4bb9ef7e89dc60c3763ec392d1ac
JenHLee <hj.jennie.lee@gmail.com>
2022-09-14 13:04:48 -0600

    create admin dashboard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import "./home.scss";
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
-import "./home.scss";
+import Widget from '../../components/widget/Widget';
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
+
+//1:05:06 부터
 
 const Home = () => {
   return (
@@ -9,8 +14,14 @@ const Home = () => {
         <Sidebar />
         <div className="homeContainer">
           <Navbar />
-          home container
-        </div>
+          <div className="widgets">
+            <Widget type="user"/>
+            <Widget type="order"/>
+            <Widget type="earning"/>
+            <Widget type="balance"/>
+          </div>
+          <div className="charts"></div>
+          </div>
     </div>
   )
 }
